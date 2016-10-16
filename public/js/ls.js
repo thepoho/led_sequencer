ac9d0117ec6d172918762e312d0b9a24280ae1c9
Peter Hofmann <thepoho@gmail.com>
2016-10-17 07:51:09 +1100

    can now drop LED's anywhere!

--- public/js/ls.js
+++ public/js/ls.js
@@ -1,5 +1,18 @@
 ls = {
 	startup: function(){
-    
+    $(".canvas").click(function(event){
+      // var offset = $(this).offset();
+      // console.log(offset);
+      // window.poho = event;
+      var offset = $(this).offset();
+      var xPos = event.clientX - offset.left - 10;
+      var yPos = event.clientY - offset.top - 10;
+
+      var tmp = $("<div class='led'></div>");
+      tmp.css("margin-left", xPos+'px');
+      tmp.css("margin-top", yPos+'px');
+      // window.poho = tmp;
+      $(".canvas").append(tmp);
+    })
   }
 }
