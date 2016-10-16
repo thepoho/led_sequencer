7952f150cc030db949ce71c7822e37b09acf0672
Peter Hofmann <thepoho@gmail.com>
2016-10-17 07:52:09 +1100

    tidy up

--- public/js/ls.js
+++ public/js/ls.js
@@ -1,9 +1,7 @@
 ls = {
 	startup: function(){
     $(".canvas").click(function(event){
-      // var offset = $(this).offset();
-      // console.log(offset);
-      // window.poho = event;
+
       var offset = $(this).offset();
       var xPos = event.clientX - offset.left - 10;
       var yPos = event.clientY - offset.top - 10;
@@ -11,7 +9,6 @@ ls = {
       var tmp = $("<div class='led'></div>");
       tmp.css("margin-left", xPos+'px');
       tmp.css("margin-top", yPos+'px');
-      // window.poho = tmp;
       $(".canvas").append(tmp);
     })
   }
