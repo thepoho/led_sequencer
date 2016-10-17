beb19d7e859a6a581bb2225020d240b9e7ad5319
Peter Hofmann <thepoho@gmail.com>
2016-10-17 17:18:23 +1100

    adding basic led object and place/edit modes

--- public/js/ls.js
+++ public/js/ls.js
@@ -1,15 +1,46 @@
 ls = {
+  mode: 'place',
+  leds: [],
 	startup: function(){
+    $("label.place_edit").click(function(){
+      ls.mode = $(this).find('input').val();
+    });
+
     $(".canvas").click(function(event){
+      if(ls.mode == 'place'){
+        ls.placeLed(event);
+      }
+    });
+  },
+  placeLed: function(event){
+    var offset = $('.canvas').offset();
+    // the -10 is because my LED is 20x20 px
+    var xPos = event.clientX - offset.left - 10;
+    var yPos = event.clientY - offset.top  - 10;
 
-      var offset = $(this).offset();
-      var xPos = event.clientX - offset.left - 10;
-      var yPos = event.clientY - offset.top - 10;
+    var led = new LED(xPos, yPos, ls.leds.length);
+    var tmp = led.getJqueryObject();
+    ls.leds.push(led);
 
-      var tmp = $("<div class='led'></div>");
-      tmp.css("margin-left", xPos+'px');
-      tmp.css("margin-top", yPos+'px');
-      $(".canvas").append(tmp);
+    $(".canvas").append(tmp);
+  },
+  redraw: function(led){
+    $('.canvas .led').remove();
+    $.each(ls.leds, function(idx, led){
+      $(".canvas").append(led.getJqueryObject());
     })
+  }
+}
+
+function LED(x,y,idx){
+  this.x = x;
+  this.y = y;
+  this.idx = idx;
+
+  this.getJqueryObject = function(){
+    var tmp = $("<div class='led'></div>");
+    tmp.css("margin-left", this.x+'px');
+    tmp.css("margin-top", this.y+'px');
+    return(tmp);
   }
 }
